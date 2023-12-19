f261062c6a3c43f1e142b618103afec9d4304f7e
whao13 <790657221@qq.com>
2023-12-19 09:47:38 +0800

    feat: 11

--- src/components/navigation/index.tsx
+++ src/components/navigation/index.tsx
@@ -1,12 +1,42 @@
-import React from 'react'
+import React, { FC } from 'react'
+import { Logo } from '../../assets/image/logo'
+import { router, ZHRouter } from '@/router'
+import { NavLink } from 'react-router-dom'
 
 type Props = {}
+interface navProps {
+  navs: ZHRouter
+}
+
+const NavTab: FC<navProps> = ({ navs }) => (
+  <div className="flex mx-6 box-border">
+    {navs.map((item) => (
+      <NavLink
+        key={item.path + '_'}
+        to={item.path || '/'}
+        className={({ isActive }) =>
+          'hover: text-black mx-4 h-full py-3.5 transition-all' +
+          (isActive
+            ? ' font-extrabold border-b-4 border-blue-600'
+            : ' text-gray-400')
+        }
+      >
+        {item.title}
+      </NavLink>
+    ))}
+  </div>
+)
 
 export default function Navigation({}: Props) {
   return (
     <div className=" bg-white shadow-lg w-screen">
-      <div className=" max-w-6xl mx-auto my-0 flex justify-center">
-        <div className=" h-14 flex justify-between items-center min-w-max"></div>
+      <div className=" max-w-6xl mx-auto my-0 flex justify-center w-full">
+        <div className=" h-14 flex justify-between items-center min-w-max w-full">
+          <div className="flex items-center">
+            <Logo />
+            <NavTab navs={router} />
+          </div>
+        </div>
       </div>
     </div>
   )
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,10 +1,16 @@
-import { routerObject, Outlet } from 'react-router-dom'
+import { RouteObject, Outlet } from 'react-router-dom'
 import React from 'react'
 import Home from '../pages/home/index'
 import Education from '../pages/education/index'
 import Learning from '../pages/education/learning/index'
 
-export const router: Array<routerObject> = [
+interface extraRouter {
+  title: string
+}
+
+export type ZHRouter = Array<RouteObject & extraRouter>
+
+export const router: ZHRouter = [
   {
     path: '/',
     title: '首页',
